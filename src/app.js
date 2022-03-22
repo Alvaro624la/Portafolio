01eba797c5655ce4ddf551eb0148d058be07ff99
Alvaro624la <alvarodepradotrabajo@gmail.com>
2022-03-22 11:03:34 +0100

    btnUP animation claslist

--- src/app.js
+++ src/app.js
@@ -9,3 +9,16 @@ function subir(){
         behavior: 'smooth'
         });
 }
+
+// Ver posicion del scroll para aparecer UPBTN (posicion 1000 aparecer)
+window.addEventListener('scroll', scroll);
+function scroll(){
+    console.log(scrollY);
+    if(scrollY <= 999){
+        UPBTN.classList.add('scroll-up-out');
+        // UPBTN.style.display = 'none';
+    } else {
+        UPBTN.classList.remove('scroll-up-out');
+        // UPBTN.style.display = 'block';
+    }
+}
